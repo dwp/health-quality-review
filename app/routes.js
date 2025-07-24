13dd1dcbcf7c2389a0b3e69557cf1ef3fee29c5e
colin passant <colin.passant@engineering.digital.dwp.gov.uk>
2025-07-24 08:11:01 +0000

    feat(htln-1234): All prototype changes made for UR
    GitOrigin-RevId: e9b7e7524227f00d55f58f5f7c70a03617dc316a

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,38 @@
+const grades = [
+  "A",
+  "AF",
+  "AA",
+  "U"
+]
+
+const badGrades = [
+  "AA",
+  "U"
+]
+
+const goodGrades = [
+  "A",
+  "AF",
+]
+
+const reviewSections = [
+  "Amendments",
+  "Feedback"
+]
+
+const groups = [
+  "evidence",
+  "infoGather",
+  "activities",
+  "mobility",
+  "opinion",
+  "process",
+  "Overallcomment"
+]
+
+const evidence = {
+  evid: "Further evidence"
+}
 
 const infoGather = {
   history: "History (including variability)",
@@ -5,11 +40,8 @@ const infoGather = {
   observation: "Observations"
 }
 
-
-
-
 const activities = {
-  food: "Preparing Food",
+  food: "Preparing food",
   nutrition: "Taking nutrution",
   therapy: "Managing therapy",
   washing: "Washing and bathing",
@@ -18,9 +50,37 @@ const activities = {
   communicating: "Communication verbally",
   reading: "Reading and understanding",
   engaging: "Engaging with others",
+  budgeting: "Making budgeting decisions"
+}
+
+
+const mobility = {
   journeys: "Planning and following journeys",
   moving: "Moving around"
 }
+
+const opinion = {
+  Prognosis: "Prognosis",
+  QPPT: "QP/PT recommendation",
+  Terminal: "Terminal illness opinion",
+  Reliability: "Reliability criteria",
+  Justification: "Justification"
+}
+
+const process = {
+  handling: "Case handling",
+  Usability: "Usability"
+}
+
+const condition = {
+  conditions: "Primary condition"
+}
+
+const Overallcomment = {
+  com: "Overall comment"
+}
+
+
 //
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
@@ -37,7 +97,38 @@ findAddressPlugin(router);
 router.get("*", (req, res, next) => {
   res.locals.query = req.query;
   res.locals.activities = activities;
+  res.locals.mobility = mobility;
   res.locals.infoGather = infoGather;
+  res.locals.evidence = evidence;
+  res.locals.opinion = opinion;
+  res.locals.process = process;
+  res.locals.Overallcomment = Overallcomment;
+  res.locals.condition = condition;
+  res.locals.reviewSections = reviewSections;
+  res.locals.grades = grades;
+  const currentGrades = Object.keys(res.locals.data)
+                      .filter((x) => x.startsWith("grade-"))
+                      .map((key) => res.locals.data[key].split("-")?.[0]?.trim());
+  const lowestGrade = grades[Math.max(...currentGrades.map((grade => grades.indexOf(grade))))]; 
+  res.locals.lowestGrade = lowestGrade;
+  res.locals.requiresAmendment = Object.keys(res.locals.data)
+                      .filter((x) => x.startsWith("grade-"))
+                      .filter((key) => badGrades.includes(res.locals.data[key].split("-")?.[0]?.trim()));
+  res.locals.groupsToAmend = groups.filter((group) => {
+    const toAmend = res.locals.requiresAmendment.map((x) => x.split("-")[1]);
+    return Object.keys(res.locals[group]).some(el => toAmend.includes(el));
+  });
+  res.locals.hasFeedback = Object.keys(res.locals.data)
+                    .filter((x) => x.startsWith("grade-"))
+                    .filter((key) => goodGrades.includes(res.locals.data[key].split("-")?.[0]?.trim()));
+  res.locals.groupsWithFeedback = groups.filter((group) => {
+    const hasFeedback = res.locals.hasFeedback.map((x) => x.split("-")[1]);
+    return Object.keys(res.locals[group]).some(el => hasFeedback.includes(el));
+  });
+  console.log("Groups with feedback", res.locals.groupsWithFeedback);
+  console.log(res.locals.hasFeedback);
+  console.log("Groups to amend", res.locals.groupsToAmend);
+  console.log(res.locals.requiresAmendment);
   return next();
 })
 
@@ -75,20 +166,11 @@ router.use('/', (req, res, next) => {
     }
 })
 
-
-
-
-
 // Supporting docs
 
-
-// const govukPrototypeKit = require('govuk-prototype-kit')
-// const router = govukPrototypeKit.requests.setupRouter()
 const versions = [
-   'htln1322-Summarylistv1'
-
+   'htln1322-Summarylistv1' 
 ]
-
 
 console.log('Setting up main router. Locating sub routers');
 versions.forEach((version) => require(`${__dirname}/views/${version}/routes/routes.js`));
@@ -98,7 +180,3 @@ router.all('*', (req, res, next) => {
     res.locals.query = req.query;
     return next();
 });
-
-
-
-
